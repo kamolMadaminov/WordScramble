79fc89ab54c85d8fc3f54d9497c3fbc5b6b000bf
developerkamol <developerkamol@gmail.com>
2025-03-27 09:25:22 +0500

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Kamol Madaminov on 27/03/25.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
